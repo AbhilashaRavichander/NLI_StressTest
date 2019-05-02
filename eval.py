7e5c085eae81ab082abb5457f4e69d9b4255da9d
Abhilasha <AbhilashaRavichander@users.noreply.github.com>
2019-05-02 12:16:07 -0400

    Restructuring with main method and enforcing jsonl format

--- eval.py
+++ eval.py
@@ -5,39 +5,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-parser = argparse.ArgumentParser(description='Read prediction file to evaluate.')
-parser.add_argument('--eval_file', dest='eval', help='File containing predictions')
-
-
-args = parser.parse_args()
-
-eval_file = args.eval
-
-competence_tests = ["antonym", "quant"]
-distraction_tests = ["negation", "wordoverlap", "length_mismatch"]
 
 def generate_report(results_dict, competence_keys, distraction_keys, noise_keys):
-	keys = results_dict.keys()
-	keys.sort()
+    '''
+    :param results_dict: model predictions on tests
+    :param competence_keys: competence tests to be reported, includes antonymy and numerical reasoning
+    :param distraction_keys: distraction tests to be reported, includes word overlap, negaion and length mismatch
+    :param noise_keys: noise tests to be reported, includes content word perturbaion (Appendix C)
+    '''
 
-	print "===================== COMPETENCE TESTS ===================== "
+    keys = results_dict.keys()
+    keys.sort()
 
-	for each_key in competence_keys:
-		print (each_key.encode('ascii','ignore')).upper().rjust(30)+" "+str(results_dict[each_key]["accuracy"]).rjust(5)
+    print("===================== COMPETENCE TESTS ===================== ")
 
-	print "\n===================== DISTRACTION TESTS ===================== "
+    for each_key in competence_keys:
+        print(each_key.encode('ascii', 'ignore')).upper().rjust(30) + " " + str(
+            results_dict[each_key]["accuracy"]).rjust(5)
 
-	for each_key in distraction_keys:
-		print (each_key.encode('ascii','ignore')).upper().rjust(30)+" "+str(results_dict[each_key]["accuracy"]).rjust(5)
+    print("\n===================== DISTRACTION TESTS ===================== ")
 
-	print "\n===================== NOISE TESTS =========================== "
+    for each_key in distraction_keys:
+        print(each_key.encode('ascii', 'ignore')).upper().rjust(30) + " " + str(
+            results_dict[each_key]["accuracy"]).rjust(5)
 
-	for each_key in noise_keys:
-		print (each_key.encode('ascii','ignore')).upper().rjust(30)+" "+str(results_dict[each_key]["accuracy"]).rjust(5)
+    print("\n===================== NOISE TESTS =========================== ")
+
+    for each_key in noise_keys:
+        print(each_key.encode('ascii', 'ignore')).upper().rjust(30) + " " + str(
+            results_dict[each_key]["accuracy"]).rjust(5)
 
 
 def plot(results_dict, competence_keys, distraction_keys, noise_keys):
-    #print results_dict.keys()
+    # print results_dict.keys()
     matched_scores = []
     mismatched_scores = []
     matched_scores.append(results_dict["mnli_dev_matched"]["accuracy"])
@@ -58,21 +58,22 @@ def plot(results_dict, competence_keys, distraction_keys, noise_keys):
     ind = np.arange(7)
     width = 0.35
     pos = range(7)
-    pos[2] = ind[2] + width/2
+    pos[2] = ind[2] + width / 2
     p1 = ax.bar(pos, matched_scores, width, color='r')
-    p2 = ax.bar(ind+width, mismatched_scores, width, color='b')
+    p2 = ax.bar(ind + width, mismatched_scores, width, color='b')
     p1[2].set_color('b')
     ax.set_title('Model Performance on all Stress Tests')
     ax.set_ylim((0.0, 100.0))
     tick_pos = ind + width / 2
     tick_pos[2] = pos[2]
     ax.set_xticks(tick_pos)
-    ax.set_xticklabels(('MultiNLI\n Dev', 'Antonymy', 'Numerical\n Reasoning', 'Word\n Overlap', 'Negation', 'Length\n Mismatch', 'Spelling\n Error'))
+    ax.set_xticklabels(('MultiNLI\n Dev', 'Antonymy', 'Numerical\n Reasoning', 'Word\n Overlap', 'Negation',
+                        'Length\n Mismatch', 'Spelling\n Error'))
     ax.legend((p1[0], p2[0]), ('Matched', 'Mismatched'))
-    #plt.show()
+    # plt.show()
 
     mismatched_scores[2] = matched_scores[2]
-    matched_scores = matched_scores[0:2]+matched_scores[3:]
+    matched_scores = matched_scores[0:2] + matched_scores[3:]
     max_matched = [74.2, 36.4, 58.3, 52.4, 63.7, 68.3]
     max_mismatched = [74.8, 32.8, 31.3, 58.4, 52.2, 65.0, 69.1]
     min_matched = [63.5, 6.3, 47.2, 39.5, 48.2, 51.1]
@@ -81,83 +82,84 @@ def plot(results_dict, competence_keys, distraction_keys, noise_keys):
     ind2 = np.arange(6)
     width2 = 0.2
     p21 = ax2.bar(ind2, min_matched, width2, color='r')
-    p22 = ax2.bar(ind2+width2, max_matched, width2, color='b')
-    p23 = ax2.bar(ind2+(2*width2), matched_scores, width2, color='g')
+    p22 = ax2.bar(ind2 + width2, max_matched, width2, color='b')
+    p23 = ax2.bar(ind2 + (2 * width2), matched_scores, width2, color='g')
     ax2.set_title('Comparison of Model Performance to Benchmarked Sentence\n Encoder Models (Matched)')
     ax2.set_ylim((0.0, 100.0))
     tick_pos2 = ind2 + width2
     ax2.set_xticks(tick_pos2)
-    ax2.set_xticklabels(('MultiNLI\n Dev', 'Antonymy', 'Word\n Overlap', 'Negation', 'Length\n Mismatch', 'Spelling\n Error'))
+    ax2.set_xticklabels(
+        ('MultiNLI\n Dev', 'Antonymy', 'Word\n Overlap', 'Negation', 'Length\n Mismatch', 'Spelling\n Error'))
     ax2.legend((p21[0], p22[0], p23[0]), ('Min Benchmark Accuracy', 'Max Benchmark Accuracy', 'Your Model Accuracy'))
 
     fig3, ax3 = plt.subplots()
     ind3 = np.arange(7)
     width3 = 0.2
     p31 = ax3.bar(ind3, min_mismatched, width3, color='r')
-    p32 = ax3.bar(ind3+width3, max_mismatched, width3, color='b')
-    p33 = ax3.bar(ind3+(2*width3), mismatched_scores, width3, color='g')
+    p32 = ax3.bar(ind3 + width3, max_mismatched, width3, color='b')
+    p33 = ax3.bar(ind3 + (2 * width3), mismatched_scores, width3, color='g')
     ax3.set_title('Comparison of Model Performance to Benchmarked Sentence\n Encoder Models (Mismatched)')
     ax3.set_ylim((0.0, 100.0))
     tick_pos3 = ind3 + width3
     ax3.set_xticks(tick_pos3)
-    ax3.set_xticklabels(('MultiNLI\n Dev', 'Antonymy', 'Numerical\n Reasoning', 'Word\n Overlap', 'Negation', 'Length\n Mismatch', 'Spelling\n Error'))
+    ax3.set_xticklabels(('MultiNLI\n Dev', 'Antonymy', 'Numerical\n Reasoning', 'Word\n Overlap', 'Negation',
+                         'Length\n Mismatch', 'Spelling\n Error'))
     ax3.legend((p31[0], p32[0], p33[0]), ('Min Benchmark Accuracy', 'Max Benchmark Accuracy', 'Your Model Accuracy'))
     plt.show()
 
 
-results_dict = {}
-if eval_file.endswith("jsonl"):
-    input = open(eval_file)
-    for line in input:
-        obj = json.loads(line)
-	if obj["source"] not in results_dict:
-	    results_dict[obj["source"]] = {"total": 0.0, "correct": 0.0, "accuracy": 0.0}
-	if obj["prediction"] == obj["gold_label"]:
-	    results_dict[obj["source"]]["correct"] += 1
-	results_dict[obj["source"]]["total"]+=1
+if __name__ == "__main__":
 
-if eval_file.endswith("txt"):
-	f = open(eval_file,"r")
-	all_lines = f.read().split("\n")
+    parser = argparse.ArgumentParser(description='Read prediction file to evaluate.')
+    parser.add_argument('--eval_file', dest='eval', help='File containing predictions')
+    parser.add_argument('--plot_report', action='store_true')
 
+    args = parser.parse_args()
 
-	for each_line in all_lines[1::]:
-		if each_line == "":
-			break
-		parts = each_line.split()
-		source = parts[-2]
-		pred_label = parts[-1]
-		gold_label = parts[0]
-		if source not in results_dict:
-			results_dict[source] = {"total": 0.0, "correct": 0.0, "accuracy": 0.0}
-		if pred_label == gold_label:
-			results_dict[source]["correct"] += 1
-		results_dict[source]["total"]+=1
+    eval_file = args.eval
 
+    competence_tests = ["antonym", "quant"]
+    distraction_tests = ["negation", "wordoverlap", "length_mismatch"]
+    noise_tests = ["dev_gram_contentword_swap_perturbed"]
 
-for each_key in results_dict:
-		results_dict[each_key]["accuracy"] = round(results_dict[each_key]["correct"] * 100.0 / results_dict[each_key]["total"],2)
+    results_dict = {}
+    if eval_file.endswith("jsonl"):
+        input = open(eval_file)
+        for line in input:
+            obj = json.loads(line)
+            if obj["source"] not in results_dict:
+                results_dict[obj["source"]] = {"total": 0.0, "correct": 0.0, "accuracy": 0.0}
+            if "prediction" in obj:
+                if obj["prediction"] == obj["gold_label"]:
+                    results_dict[obj["source"]]["correct"] += 1
+            results_dict[obj["source"]]["total"] += 1
 
-keys = results_dict.keys()
-keys.sort()
+    for each_key in results_dict:
+        results_dict[each_key]["accuracy"] = round(
+            results_dict[each_key]["correct"] * 100.0 / results_dict[each_key]["total"], 2)
 
-competence_keys = []
-for each_test in competence_tests:
-	for each_key in keys:
-		if each_key.startswith(each_test):
-			competence_keys.append(each_key)
+    keys = results_dict.keys()
+    keys.sort()
 
-distraction_keys = []
-for each_test in distraction_tests:
-	for each_key in keys:
-		if each_key.startswith(each_test):
-			distraction_keys.append(each_key)
+    competence_keys = []
+    for each_test in competence_tests:
+        for each_key in keys:
+            if each_key.startswith(each_test):
+                competence_keys.append(each_key)
 
-noise_keys = []
-for each_key in keys:
-	if "gram" in each_key:
-		noise_keys.append(each_key)
+    distraction_keys = []
+    for each_test in distraction_tests:
+        for each_key in keys:
+            if each_key.startswith(each_test):
+                distraction_keys.append(each_key)
 
+    noise_keys = []
+    for each_test in noise_tests:
+        for each_key in keys:
+            if each_key.startswith(each_test):
+                noise_keys.append(each_key)
 
-generate_report(results_dict, competence_keys, distraction_keys, noise_keys)
-plot(results_dict, competence_keys, distraction_keys, noise_keys)
+    generate_report(results_dict, competence_keys, distraction_keys, noise_keys)
+
+    if args.plot_report:
+        plot(results_dict, competence_keys, distraction_keys, noise_keys)
